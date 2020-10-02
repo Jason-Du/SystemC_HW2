d1bed85a1216dfbd72d3d5749036f2043fd555b9
“Jason-Du(NCKU) <53274895+Jason-Du@users.noreply.github.com>
2020-10-02 22:21:07 +0800

    upload file

---
+++ HW2_1.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include<stdlib.h>
+#define TEXT_SIZE 100
+void calculate(char filename[20])
+{
+	 int upper;
+	 int lines;
+	 int words;
+	 int characters;
+	 char ch;
+	 FILE *file;
+	characters = words = lines =upper= 0;
+	 file = fopen(filename,"r");
+	 
+	 if (file==NULL) 
+    { 
+        printf("no such file.");
+    }
+	else
+	{
+		while (   (     ch = fgetc(file)   ) != EOF)
+		{
+			characters++;
+			
+			if (ch == '\n'|| ch == '\0')
+				lines++;
+
+		   
+			if (ch == ' ' || ch == '\t' || ch == '\n'|| ch == '\0')
+				words++;
+			
+			if (ch!= ' ')
+			{
+				if (ch>= 'A' && ch <= 'Z')
+				 upper++;
+			}
+		}
+
+		if (characters > 0)
+		{
+			words++;
+			lines++;
+		}
+
+		printf("\n");
+		printf("Total characters = %d\n", characters);
+		printf("Total words      = %d\n", words);
+		printf("Total lines      = %d\n", lines);
+		printf("Uppercase Letters : %d\n", upper);
+
+		fclose(file);
+	}	
+}
+
+void makefile(char filename[20])
+{
+	char s[TEXT_SIZE];
+	char ch;
+	int i;
+	FILE *file;
+	file = fopen(filename,"r");
+	i=0;
+	if (file==NULL) 
+	{ 
+        printf("no such file.");
+    }
+
+	else
+	{
+		while (   (     ch = fgetc(file)   ) != EOF)
+		{
+			//printf("%c\n",ch);
+			if(ch!= ' ')
+			{
+				if (ch>= 'a' && ch <= 'z')
+				{
+					s[i]=ch-32;
+					//printf("%c\n",s[i]);
+				}
+				else
+					s[i]=ch;	
+				i++;
+			
+			}
+			else
+			{
+				s[i]=ch;
+				i++;
+			}
+		}
+		//printf("%s",s);
+		fclose(file);
+		file = fopen("UPPER","w");
+		fprintf(file,"%s",s);
+		fclose(file);
+	
+	}
+}
+ int main(int argc, char *argv[]) 
+ {
+	calculate(argv[2]);
+	makefile(argv[2]);
+	return 0;
+	
+ }
